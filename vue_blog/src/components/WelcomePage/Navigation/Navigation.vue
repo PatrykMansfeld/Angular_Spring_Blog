cab10cee4d3451c1f32a76c048f868aba8c702ca
Patryk Mansfeld <patryk.mansfeld@gmail.com>
2024-04-07 21:54:35 +0200

    Update Navigation.vue

--- vue_blog/src/components/WelcomePage/Navigation/Navigation.vue
+++ vue_blog/src/components/WelcomePage/Navigation/Navigation.vue
@@ -2,10 +2,11 @@
     <nav>
         <ul>
             <li><router-link to="/">Home</router-link></li>
-            <li><router-link to="/posts">Posts</router-link></li>
+            <li><router-link to="/blog">Posts</router-link></li>
             <li><router-link to="/about">About</router-link></li>
         </ul>
     </nav>
+    <router-view/>
 </template>
 
 
