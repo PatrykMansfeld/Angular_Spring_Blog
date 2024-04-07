ee09aceec18b090f9914ce88b05e8ce694c1feb4
Patryk Mansfeld <patryk.mansfeld@gmail.com>
2024-04-07 16:47:50 +0200

    Update router.js

--- vue_blog/src/router.js
+++ vue_blog/src/router.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import WelcomePageVue from '../components/WelcomePage.vue'
-import BlogApiVue from '../components/BlogApi.vue'
+import WelcomePageVue from './components/WelcomePage.vue'
+import BlogApiVue from './components/BlogApi.vue'
 
 const routes = [
     { path: '/', component: WelcomePageVue },
