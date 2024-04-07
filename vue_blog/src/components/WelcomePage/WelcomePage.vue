6e6474a5f4c14e7740b911a9994bc7ebac4dd450
Patryk Mansfeld <patryk.mansfeld@gmail.com>
2024-04-07 10:23:37 +0200

    Basic_WelcomePage_Layout

---
+++ vue_blog/src/components/WelcomePage/WelcomePage.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Navigation from './Navigation/Navigation.vue';
+</script>
+
+<template>
+    <div class="welcomePage_Component">
+        <Navigation />
+        <h1>Welcome to The.Blog</h1>
+    </div>
+</template>
+
+
+<style scoped>
+.welcomePage_Component {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+</style> 
